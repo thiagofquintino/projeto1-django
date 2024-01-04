04c051e95e3c5aa6380e0e61c950c591070b94ad
Thiago Quintino <thiagofquintino@gmail.com>
2024-01-03 23:15:08 -0300

    conhecendo as urls

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):    
+    return HttpResponse('HOME 1')
+
+def contato(request):
+    return HttpResponse('contato')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
