ec2939d627bf5dcc4f98aa67db42ab1a2b3c7d9a
Thiago Quintino <thiagofquintino@gmail.com>
2024-01-05 15:12:46 -0300

    removendo contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):    
-    return HttpResponse('HOME 1')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Thiago Quintino',
+    })
 
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
