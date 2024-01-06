f427c7baf5a4bf6fafc18389c728b5ae759b9e90
Thiago Quintino <thiagofquintino@gmail.com>
2024-01-05 23:40:57 -0300

    Separando as páginas html

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):    
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Thiago Quintino',
     })
 
